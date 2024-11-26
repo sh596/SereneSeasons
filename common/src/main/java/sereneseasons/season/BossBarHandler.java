bc71f1d323f3a602e6fbe0c56657e9f48c323f32
hadasjk <36807086+hadasjk@users.noreply.github.com>
2024-11-26 12:48:10 +0900

    feat: bossbar

--- common/src/main/java/sereneseasons/season/BossBarHandler.java
+++ common/src/main/java/sereneseasons/season/BossBarHandler.java
@@ -1,4 +1,22 @@
 package sereneseasons.season;
 
+import glitchcore.event.TickEvent;
+import glitchcore.event.player.PlayerEvent;
+import net.minecraft.server.level.ServerPlayer;
+import sereneseasons.bossbar.BossBar;
+
 public class BossBarHandler {
+
+    public static float progress = 0;
+    public static void onJoinLevel(PlayerEvent.JoinLevel event) {
+        if(event.getPlayer() instanceof ServerPlayer serverPlayer){
+            BossBar.showBossBar(serverPlayer);
+        }
+    }
+    public static void onWorldTick(TickEvent.Level event){
+        progress += 0.0001f;
+        BossBar.updateBossBar(progress);
+
+        //event.getLevel().tickRateManager().setTickRate(1);
+    }
 }
